d3bcf4973c638f71a1f26c0053dc487c49354620
monthebikeck-a11y <monthebikeck@gmail.com>
2026-09-03 11:21:00 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Frontend configuration.
-// Keep CinetPay secrets ONLY on the backend.
-const STORE_CONFIG = {
-  currency: "XAF",
-  cinetpay: {
-    enabled: true,
-    apiEndpoint: "https://TON-BACKEND.com/api/create-payment"
-  }
-};
